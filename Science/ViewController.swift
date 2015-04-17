2cf98890932766185655bdb6aa6f2700267f62af
Jamone Kelly <xg.monie089@gmail.com>
2015-04-17 10:55:11 -0400

    Initial Commit

---
+++ Science/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Science
+//
+//  Created by Jamone Alexander Kelly on 4/17/15.
+//  Copyright (c) 2015 Pixels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
